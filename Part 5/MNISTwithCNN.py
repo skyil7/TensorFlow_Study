1810649b32653a5b976ea75e63250e12aca3cef0
GIO_LAB <giopaik@sju.ac.kr>
2019-08-20 20:46:45 +0900

    commit

--- Part 5/MNISTwithCNN.py
+++ Part 5/MNISTwithCNN.py
@@ -4,6 +4,11 @@ import numpy as np
 from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets("./mnist/data/", one_hot=True)
 
+#Hyperparam
+learning_rate=0.01
+batch_size = 500
+epoch = 30
+
 X = tf.placeholder(tf.float32, [None, 28, 28, 1])#28*28 이미지, 1은 특징의 개수로, MNIST에서는 회색조 이미지라 색상이 하나 뿐이므로 1
 Y = tf.placeholder(tf.float32, [None, 10])
 keep_prob = tf.placeholder(tf.float32)
@@ -34,7 +39,7 @@ with tf.name_scope('layer4'):
     model = tf.matmul(L3,W4)
 
 cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=model, labels=Y))
-optimizer = tf.train.AdamOptimizer(0.001).minimize(cost)
+optimizer = tf.train.AdamOptimizer(learning_rate).minimize(cost)
 #나중에 아래 옵티마이저로도 해보자.
 #optimizer = tf.train.RMSPropOptimizer(0.001, 0.9).minimize(cost)
 
@@ -46,10 +51,9 @@ init = tf.global_variables_initializer()
 sess = tf.Session()
 sess.run(init)
 
-batch_size = 500
 total_batch = int(mnist.train.num_examples / batch_size)
 
-for epoch in range(15):
+for e in range(epoch):
     total_cost = 0
     for i in range(total_batch):
         batch_xs, batch_ys = mnist.train.next_batch(batch_size)
@@ -59,7 +63,7 @@ for epoch in range(15):
 
         total_cost+=cost_val
 
-    print('Epoch:','%04d' %(epoch+1), 'Avg. cost =','{:3f}'.format(total_cost/total_batch))
+    print('Epoch:','%04d/%04d' %(e+1,epoch), 'Avg.cost =','{:3f}'.format(total_cost/total_batch))
 
 print('Optimize Complete!')
 
